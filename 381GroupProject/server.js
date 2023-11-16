e5945e0178b0e0921d7e805e1f3b9dc9f2e9fb7f
DengHaoyang97 <49272807+DengHaoyang97@users.noreply.github.com>
2023-11-17 05:25:16 +0800

    Update server.js

--- 381GroupProject/server.js
+++ 381GroupProject/server.js
@@ -142,7 +142,7 @@ app.get('/', async(req, res) => {
     try {   
 	await awmongoose.connect(`mongodb+srv://billydeng97:dhy97886886@cluster0.zsgzyzj.mongodb.net/381Project?retryWrites=true&w=majority`);
 	console.log('list books');
-        const books = await Book.find();
+	    const books= await Book.find();;
         res.render('index', { books: books });
 	await mongoose.disconnect();	
 	    
